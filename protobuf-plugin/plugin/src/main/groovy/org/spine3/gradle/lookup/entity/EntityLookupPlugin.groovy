6f31d652e3fd95d0c1749921a63f732419375d36
Alexander Litus <alexander.litus@teamdev.com>
2016-04-04 11:01:42 +0300

    Add entity lookup plugin (searching entity packages).

---
+++ protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/lookup/entity/EntityLookupPlugin.groovy
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.gradle.lookup.entity
+
+import groovy.util.logging.Slf4j
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+
+import static ProtoParser.ProtoFileMetadata
+
+/**
+ * Finds packages with Protobuf files belonging to an entity (containing entity state, commands, etc)
+ * and writes each package and the type of the entity to which it belongs to the {@code entities.properties} file.
+ *
+ * @author Alexander Litus
+ */
+@Slf4j
+public class EntityLookupPlugin implements Plugin<Project> {
+
+    private static final String PROPS_FILE_NAME = "entities.properties";
+
+    private Project project;
+
+    @Override
+    void apply(Project project) {
+        this.project = project;
+        final Task findEntitiesTask = project.task("findEntities") {
+            findEntities();
+        }
+        findEntitiesTask.dependsOn("compileJava", "generateProto", "generateTestProto");
+        final Task processResources = project.getTasks().getByPath("processResources");
+        processResources.dependsOn(findEntitiesTask);
+    }
+
+    private void findEntities() {
+        findEntityFilesAndWriteProps("main");
+        findEntityFilesAndWriteProps("test");
+    }
+
+    private void findEntityFilesAndWriteProps(String mainOrTest) {
+        final String projectPath = project.getProjectDir().getAbsolutePath();
+        final EntityFilesFinder finder = new EntityFilesFinder(project, "${projectPath}/src/${mainOrTest}/proto");
+        final List<ProtoFileMetadata> metadataList = finder.findFiles();
+        if (metadataList.isEmpty()) {
+            return;
+        }
+        final Map<String, String> propsMap = toPackagesMap(metadataList)
+        final PropertiesWriter writer = new PropertiesWriter("${projectPath}/generated/${mainOrTest}/resources", PROPS_FILE_NAME);
+        writer.write(propsMap);
+    }
+
+    private static Map<String, String> toPackagesMap(List<ProtoFileMetadata> metadataList) {
+        final Map<String, String> result = new HashMap<>();
+        for (ProtoFileMetadata metadata : metadataList) {
+            final String pack = metadata.getProtoPackage();
+            final String type = metadata.getEntityType();
+            result.put(pack, type);
+        }
+        return result;
+    }
+}
---
+++ protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/lookup/entity/ProtoParser.groovy
@@ -0,0 +1,131 @@
+package org.spine3.gradle.lookup.entity
+
+/**
+ * A utility class for parsing {@code .proto} files.
+ *
+ * @author Alexander Litus
+ */
+public class ProtoParser {
+
+    private static final String COMMENT = "//";
+    private static final String PACKAGE = "package";
+    public static final String STATE_OF_ENTITY_OPTION = "option(state_of)=";
+
+    private ProtoParser() {}
+
+    /**
+     * A copy of Spine EntityType enum defined in Protobuf.
+     * An original enum is not used here to avoid circular dependencies between modules.
+     */
+    private static enum EntityType {
+        NOT_SET,
+        AGGREGATE,
+        PROCESS_MANAGER,
+        PROJECTION;
+    }
+
+    /**
+     * Parses the {@code .proto} file and returns its metadata.
+     *
+     * @param file a {@code .proto} file to parse
+     * @return a file metadata
+     */
+    public static ProtoFileMetadata parse(File file) {
+        String protoPackage = "";
+        EntityType entityType = EntityType.NOT_SET;
+        final List<String> lines = file.readLines();
+        for (String fullLine : lines) {
+            String line = trimAndRemoveComments(fullLine)
+            if (line.isEmpty()) {
+                continue;
+            }
+            if (line.startsWith(PACKAGE)) {
+                protoPackage = parseProtoPackage(line);
+            } else {
+                if (containsEntityTypeOption(line)) {
+                    entityType = parseEntityType(line);
+                    if (!protoPackage.isEmpty()) {
+                        break;
+                    }
+                }
+            }
+        }
+        final ProtoFileMetadata metadata = new ProtoFileMetadata(protoPackage, entityType, file.getPath());
+        return metadata;
+    }
+
+    private static String trimAndRemoveComments(String fullLine) {
+        String result = fullLine.trim();
+        if (result.contains(COMMENT)) {
+            result = result.substring(0, result.indexOf(COMMENT));
+        }
+        return result;
+    }
+
+    private static String parseProtoPackage(String line) {
+        final int packageStartIndex = line.indexOf(' ') + 1;
+        final int semicolonIndex = line.indexOf(';');
+        final String protoPackage = line.substring(packageStartIndex, semicolonIndex);
+        return protoPackage;
+    }
+
+    private static boolean containsEntityTypeOption(String line) {
+        final String lineNoSpaces = line.replace(' ', '');
+        final boolean contains = lineNoSpaces.contains(STATE_OF_ENTITY_OPTION);
+        return contains;
+    }
+
+    private static EntityType parseEntityType(String line) {
+        for (EntityType entityType : EntityType.values()) {
+            if (line.contains(entityType.toString())) {
+                return entityType;
+            }
+        }
+        return EntityType.NOT_SET;
+    }
+
+    /**
+     * A {@code .proto} file metadata.
+     */
+    public static class ProtoFileMetadata {
+
+        private final String protoPackage;
+        private final String entityType;
+        private final String fileName;
+
+        private ProtoFileMetadata(String protoPackage, EntityType entityType, String fileName) {
+            this.protoPackage = protoPackage;
+            this.entityType = entityType.toString();
+            this.fileName = fileName;
+        }
+
+        /**
+         * Returns a Protobuf package defined in the file.
+         */
+        public String getProtoPackage() {
+            return protoPackage;
+        }
+
+        /**
+         * Returns an entity type string defined via {@code state_of} option in any message from the file.
+         */
+        public String getEntityType() {
+            return entityType;
+        }
+
+        /**
+         * Returns a name of the file.
+         */
+        public String getFileName() {
+            return fileName;
+        }
+
+        /**
+         * Returns true if the {@code state_of} option is set in any message defined in the file, false otherwise.
+         */
+        public boolean isEntityStateDefined() {
+            final boolean isSet = entityType != EntityType.NOT_SET.toString();
+            return isSet;
+        }
+    }
+}
