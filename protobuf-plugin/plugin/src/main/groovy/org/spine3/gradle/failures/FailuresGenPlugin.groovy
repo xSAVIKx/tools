27e6d0572ddedb6a123543056838cec90e0edd65
Mikhail Mikhaylov <mikhail.mikhaylov@teamdev.com>
2016-04-27 14:29:00 +0300

    Correct javadoc.

--- protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/failures/FailuresGenPlugin.groovy
+++ protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/failures/FailuresGenPlugin.groovy
@@ -10,7 +10,7 @@ import org.gradle.api.Task
  * Plugin, which generates Failures, based on failures.proto files.
  *
  * <p>Uses generated proto descriptors.
- * <p>Generates warning if there are no protobuf descriptors generated.
+ * <p>Logs a warning if there are no protobuf descriptors generated.
  */
 @Slf4j
 class FailuresGenPlugin implements Plugin<Project> {
