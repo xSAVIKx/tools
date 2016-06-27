533d9bab5597b5613724f847f3b4d1ba7db113d2
Alexander Litus <alexander.litus@teamdev.com>
2016-06-27 17:37:05 +0300

    Add enrichments lookup plugin.

---
+++ protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/lookup/enrichments/EnrichmentLookupPlugin.groovy
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2015, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.gradle.lookup.enrichments
+
+import groovy.util.logging.Slf4j
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.Task
+import org.spine3.gradle.lookup.entity.PropertiesWriter
+
+import static com.google.protobuf.DescriptorProtos.FileDescriptorProto
+import static com.google.protobuf.DescriptorProtos.FileDescriptorSet
+
+/**
+ * Finds event enrichment Protobuf definitions; creates a {@code .properties} file, which contains entries like:
+ *
+ * <p>{@code ENRICHMENT_TYPE_NAME=EVENT_TO_ENRICH_TYPE_NAME.}
+ *
+ * @author Alexander Litus
+ */
+@Slf4j
+public class EnrichmentLookupPlugin implements Plugin<Project> {
+
+    private static final String PROPS_FILE_NAME = "enrichments.properties";
+
+    public static final String MSG_ENABLE_DESCRIPTOR_SET_GENERATION =
+            "Please enable descriptor set generation. See an appropriate section at " +
+            "https://github.com/google/protobuf-gradle-plugin/blob/master/README.md#customize-code-generation-tasks"
+
+    private Project project;
+    private String projectPath;
+
+    @Override
+    void apply(Project project) {
+        this.project = project;
+        this.projectPath = project.getProjectDir().getAbsolutePath();
+        final Task findEnrichmentsTask = project.task("findEnrichments") << {
+            findEnrichmentsAndWriteProps("main", true);
+        }
+        findEnrichmentsTask.dependsOn("compileJava");
+        final Task processResources = project.getTasks().getByPath("processResources");
+        processResources.dependsOn(findEnrichmentsTask);
+
+        final Task findTestEnrichmentsTask = project.task("findTestEnrichments") << {
+            findEnrichmentsAndWriteProps("test", false);
+        }
+        findTestEnrichmentsTask.dependsOn("compileTestJava");
+        final Task processTestResources = project.getTasks().getByPath("processTestResources");
+        processTestResources.dependsOn(findTestEnrichmentsTask);
+    }
+
+    private void findEnrichmentsAndWriteProps(String mainOrTest, boolean enableWarn) {
+        final Map<String, String> propsMap = new HashMap<>();
+        final List<FileDescriptorProto> files = getFileDescriptors(mainOrTest, enableWarn);
+        for (FileDescriptorProto file : files) {
+            final Map<String, String> enrichments = new EnrichmentsFinder(file).findEnrichments();
+            propsMap.putAll(enrichments);
+        }
+        if (propsMap.isEmpty()) {
+            return;
+        }
+        final PropertiesWriter writer = new PropertiesWriter(
+                "${projectPath}/generated/${mainOrTest}/resources",
+                PROPS_FILE_NAME);
+        writer.write(propsMap);
+    }
+
+    private List<FileDescriptorProto> getFileDescriptors(String mainOrTest, boolean enableWarn) {
+        final String descFilePath = "${projectPath}/build/descriptors/${mainOrTest}.desc";
+        if (!new File(descFilePath).exists()) {
+            if (enableWarn) {
+                log.warn(MSG_ENABLE_DESCRIPTOR_SET_GENERATION);
+            }
+            return Collections.emptyList();
+        }
+        final List<FileDescriptorProto> fileDescriptors = new LinkedList<>();
+        new FileInputStream(descFilePath).withStream {
+            final FileDescriptorSet descriptorSet = FileDescriptorSet.parseFrom(it);
+            descriptorSet.fileList.each { FileDescriptorProto descriptor ->
+                fileDescriptors.add(descriptor);
+            }
+        }
+        return fileDescriptors;
+    }
+}
---
+++ protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/lookup/enrichments/EnrichmentsFinder.groovy
@@ -0,0 +1,137 @@
+/*
+ * Copyright 2015, TeamDev Ltd. All rights reserved.
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.spine3.gradle.lookup.enrichments
+import groovy.util.logging.Slf4j
+
+import static com.google.protobuf.DescriptorProtos.*
+
+/**
+ * Finds event enrichment Protobuf definitions.
+ *
+ * @author Alexander Litus
+ */
+@Slf4j
+public class EnrichmentsFinder {
+
+    /**
+     * The field number of the message option `enrichment_for` defined in `Spine/core-java`.
+     */
+    private static final String OPTION_FIELD_NUMBER_ENRICHMENT_FOR = String.valueOf(57124);
+
+    /**
+     * The field number of the field option `by` defined in `Spine/core-java`.
+     */
+    private static final String OPTION_FIELD_NUMBER_ENRICH_BY = String.valueOf(57125);
+
+    public static final String QUOTE = '"';
+
+    public static final String PROTO_TYPE_SEPARATOR = '.';
+
+    private final FileDescriptorProto file;
+    private final String packagePrefix;
+
+    /**
+     * Creates a new instance.
+     *
+     * @param file a file to search enrichments in
+     */
+    public EnrichmentsFinder(FileDescriptorProto file) {
+        this.file = file;
+        this.packagePrefix = file.getPackage() + PROTO_TYPE_SEPARATOR;
+    }
+
+    /**
+     * Finds event enrichment Protobuf definitions in the file.
+     *
+     * @return a map from enrichment type name to event to enrich type name
+     */
+    public Map<String, String> findEnrichments() {
+        // Do not name this method "find" to avoid a confusion with "DefaultGroovyMethods.find()".
+        final Map<String, String> result = new HashMap<>();
+        final List<DescriptorProto> messages = file.getMessageTypeList();
+        for (DescriptorProto msg : messages) {
+            final String targetEventName = getEventToEnrichName(msg)
+            if (targetEventName != null) {
+                String enrichmentName = packagePrefix + msg.getName();
+                result.put(enrichmentName, targetEventName);
+            }
+        }
+        return result;
+    }
+
+    private String getEventToEnrichName(DescriptorProto msg) {
+        final String eventToEnrichName = parseEventToEnrichName(msg);
+        if (eventToEnrichName != null) {
+            return eventToEnrichName;
+        }
+        for (FieldDescriptorProto field : msg.getFieldList()) {
+            if (hasOptionEnrichBy(field)) {
+                final String enrichmentName = msg.getName();
+                final String outerTypeName = getOuterTypeName(enrichmentName)
+                if (outerTypeName == null || containsOnlyLowercaseChars(outerTypeName)) {
+                    logErrInvalidEnrichmentDeclaration(enrichmentName);
+                    return null;
+                }
+                return outerTypeName;
+            }
+        }
+        return null;
+    }
+
+    private static String getOuterTypeName(String enrichmentName) {
+        final int lastDotIndex = enrichmentName.lastIndexOf(PROTO_TYPE_SEPARATOR);
+        if (lastDotIndex < 0) {
+            return null;
+        }
+        final String outerTypeName = enrichmentName.substring(0, lastDotIndex);
+        return outerTypeName;
+    }
+
+    private static boolean containsOnlyLowercaseChars(String outerTypeName) {
+        boolean result = outerTypeName.toLowerCase().equals(outerTypeName);
+        return result
+    }
+
+    private static logErrInvalidEnrichmentDeclaration(String enrichmentName) {
+        log.error("Event enrichment message $enrichmentName must have 'enrichment_for' " +
+                "Protobuf option, or it must be an inner type of the event to enrich.");
+    }
+
+    private String parseEventToEnrichName(DescriptorProto msg) {
+        // This option is "unknown" and serialized, but it is possible to print option's field number and value.
+        String optionsStr = msg.getOptions().getUnknownFields().toString();
+        if (!optionsStr.contains(OPTION_FIELD_NUMBER_ENRICHMENT_FOR)) {
+            return null;
+        }
+        String msgName = optionsStr.substring(optionsStr.indexOf(QUOTE) + 1, optionsStr.lastIndexOf(QUOTE));
+        if (!msgName.contains(packagePrefix)) {
+            msgName = packagePrefix + msgName;
+        }
+        return msgName;
+    }
+
+    private static boolean hasOptionEnrichBy(FieldDescriptorProto field) {
+        // This option is "unknown" and serialized, but it is possible to print option's field number and value.
+        final String optionsStr = field.getOptions().getUnknownFields().toString();
+        final boolean result = optionsStr.contains(OPTION_FIELD_NUMBER_ENRICH_BY);
+        return result;
+    }
+}
