3f9cfbe91e1274f12a5bf744073fbac7d2c761c5
Alexander Litus <alexander.litus@teamdev.com>
2016-07-28 15:10:26 +0300

    Remove test project dir because it pollutes the project.

--- protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/protobuf/failures/FailuresGenPlugin.groovy
+++ protobuf-plugin/plugin/src/main/groovy/org/spine3/gradle/protobuf/failures/FailuresGenPlugin.groovy
@@ -19,6 +19,7 @@
  */
 
 package org.spine3.gradle.protobuf.failures
+
 import groovy.util.logging.Slf4j
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -27,6 +28,7 @@ import org.gradle.api.Task
 import static com.google.protobuf.DescriptorProtos.*
 import static org.spine3.gradle.protobuf.Extension.*
 import static org.spine3.gradle.protobuf.util.DescriptorSetUtil.getProtoFileDescriptors
+
 /**
  * Plugin which generates Failures, based on failures.proto files.
  *
