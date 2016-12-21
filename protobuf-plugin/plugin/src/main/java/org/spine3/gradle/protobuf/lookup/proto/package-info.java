fb8186cdfe883c481c8b3ca900c6b0064ba5c64d
Alex Tymchenko <oleksii.tymchenko@teamdev.com>
2016-12-21 18:56:38 +0200

    Separate a copyright header from the JavaDoc section in `package-info`s across the project.

--- protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/lookup/proto/package-info.java
+++ protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/lookup/proto/package-info.java
@@ -16,11 +16,11 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
+ */
+
+/**
  * Plugins and utilities for Protobuf-to-Java type mapping.
  *
  * @author Alex Tymchenko
- *
  */
 package org.spine3.gradle.protobuf.lookup.proto;
--- protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/package-info.java
+++ protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/package-info.java
@@ -16,8 +16,9 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- *
+ */
+
+/**
  * Spine Gradle plugins for code generation and build-time Protobuf model validation.
  *
  * @author Alex Tymchenko
