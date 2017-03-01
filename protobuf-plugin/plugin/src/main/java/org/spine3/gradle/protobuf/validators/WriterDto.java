ed7ee809d3f5928ca37bb22c50e4390bc61659fb
illiashepilov <illia.shepilov@teamdev.com>
2017-03-01 11:14:44 +0200

    Generate `equals` and `hashCode` for `WriterDto`.

--- protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/validators/WriterDto.java
+++ protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/validators/WriterDto.java
@@ -1,5 +1,6 @@
 package org.spine3.gradle.protobuf.validators;
 
+import com.google.common.base.Objects;
 import com.google.protobuf.DescriptorProtos;
 
 /**
@@ -28,5 +29,24 @@ public class WriterDto {
 
     public DescriptorProtos.DescriptorProto getMsgDescriptor() {
         return msgDescriptor;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        WriterDto dto = (WriterDto) o;
+        return Objects.equal(javaPackage, dto.javaPackage) &&
+                Objects.equal(javaClass, dto.javaClass) &&
+                Objects.equal(msgDescriptor, dto.msgDescriptor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(javaPackage, javaClass, msgDescriptor);
     }
 }
