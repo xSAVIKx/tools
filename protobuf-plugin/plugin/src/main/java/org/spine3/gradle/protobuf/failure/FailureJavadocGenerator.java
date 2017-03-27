1c38b9abcc02609d016a0d7e526d376b592c38da
dmytro.grankin <dmytro.grankin@teamdev.com>
2017-03-27 18:28:07 +0300

    Remove unused imports

--- protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/failure/FailureJavadocGenerator.java
+++ protobuf-plugin/plugin/src/main/java/org/spine3/gradle/protobuf/failure/FailureJavadocGenerator.java
@@ -35,7 +35,6 @@ import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
